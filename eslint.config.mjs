a2753168730a7ee03d3f25623d4d3729de86a811
vladimirugol <vladimirugol66@gmail.com>
2025-10-16 14:42:37 +0300

    АААААААААААААААААААААААААААААААА

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },{rules: {"no-unused-vars": "warn"}},
   tseslint.configs.recommended,
 ]);
